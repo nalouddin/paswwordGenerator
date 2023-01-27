678ed1a9246c329c447d93506af68441b46328b9
nalouddin <nalouddin@gmail.com>
2023-01-27 12:33:08 +0500

    Yaxshi ishlamayabdi pasword genitarsiyasi

--- passwordGenerator/urls.py
+++ passwordGenerator/urls.py
@@ -1,4 +1,4 @@
-"""passwordGenerator URL Configuration
+"""password_generator URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from generator import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.home),
+    path('password', views.password, name='password'),
 ]
