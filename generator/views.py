90669462b3eb43729e88f751c9e9827134c54554
nalouddin <nalouddin@gmail.com>
2023-01-27 12:48:08 +0500

    PaswordGenerator project contuniy

--- generator/views.py
+++ generator/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import random
+
 # Create your views here.
 
 def home(request):
     return render(request, 'generator/home.html')
+
+def about(request):
+    return render(request, 'generator/about.html')
 
 def password(request):
     characters = list('abcdefghijklmnopqrstuvwxyz')
@@ -16,7 +20,7 @@ def password(request):
     if request.GET.get('numbers'):
         characters.extend(list('0123456789'))
 
-    length = int(request.GET.get('length', 12))
+    length = int(request.GET.get('length',12))
 
     thepassword = ''
     for x in range(length):
